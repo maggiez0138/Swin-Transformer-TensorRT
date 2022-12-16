43a6055c109f15bfc48346760d2bb501bdda6303
Weihua Zhang <weihuaz@nvidia.com>
2022-12-16 11:41:45 +0800

    update export.py

--- export.py
+++ export.py
@@ -51,10 +51,13 @@ def parse_option():
                         help='no: no cache, '
                              'full: cache all data, '
                              'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
+    parser.add_argument('--pretrained',
+                        help='pretrained weight from checkpoint, could be imagenet22k pretrained weight')
     parser.add_argument('--resume', default='./weights/swin_tiny_patch4_window7_224.pth', help='resume from checkpoint')
     parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
     parser.add_argument('--use-checkpoint', action='store_true',
                         help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--disable_amp', action='store_true', help='Disable pytorch amp')
     parser.add_argument('--amp-opt-level', type=str, default='O1', choices=['O0', 'O1', 'O2'],
                         help='mixed precision opt level, if O0, no amp is used')
     parser.add_argument('--output', default='output', type=str, metavar='PATH',
@@ -63,11 +66,17 @@ def parse_option():
     parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
     parser.add_argument('--throughput', action='store_true', help='Test throughput only')
 
+    # If it is a QAT-wrapped model
+    parser.add_argument('--quantize', action='store_true', help='enable QAT wrapper')
+
     # # settings for exporting onnx
     # parser.add_argument('--batch-size-onnx', type=int, help="batchsize when export the onnx model")
 
     # distributed training
     parser.add_argument("--local_rank", type=int, help='local rank for DistributedDataParallel')
+    # for acceleration
+    parser.add_argument('--fused_window_process', action='store_true',
+                        help='Fused window shift & window partition, similar for reversed part.')
 
     args, unparsed = parser.parse_known_args()
     config = get_config(args)
@@ -102,11 +111,14 @@ def export_onnx(model, config):
         print('ONNX export failure: %s' % e)
 
 
-def main(config):
+def main(config, args):
     logger.info(f"Creating model:{config.MODEL.TYPE}/{config.MODEL.NAME}")
-    model = build_model(config)
+    model = build_model(config, args.quantize)
     model.cuda()
     logger.info(str(model))
+
+    # import torchinfo
+    # torchinfo.summary(model, (1, 3, 224, 224), depth=5)
 
     checkpoint = torch.load(config.MODEL.RESUME, map_location='cpu')
     msg = model.load_state_dict(checkpoint['model'] if 'model' in checkpoint.keys() else checkpoint,
@@ -118,7 +130,7 @@ def main(config):
 
 
 if __name__ == '__main__':
-    _, config = parse_option()
+    args, config = parse_option()
 
     os.makedirs(config.OUTPUT, exist_ok=True)
     logger = create_logger(output_dir=config.OUTPUT, dist_rank=0, name=f"{config.MODEL.NAME}")
@@ -126,4 +138,4 @@ if __name__ == '__main__':
     # print config
     logger.info(config.dump())
 
-    main(config)
+    main(config, args)
